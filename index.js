ac2e3617594a04630bbe28dc8a2fccf6be978c92
Jesse Miller <silverhonda84@gmail.com>
2025-05-04 09:54:10 -0700

    Final Module 4 Project
    Final submission

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-const moviesListEl = document.querySelector(".movies-list");
-const id = localStorage.getItem("id")
+const moviesListEl = document.querySelector(".movies");
 
 function onSearchChange(event) {
-  event.preventDefault()  
-  const id = document.querySelector(searchForm);
-    renderMovies(id)
+    const searchTerm = event.target.value;
+    renderMovies(searchTerm);
 }
 
-async function renderMovies(id) {
-  const movies = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=94c6a856&s=${id}`
-  );
-  const moviesData = await movies.json();
-  console.log(moviesData);
-  moviesListEl.innerHTML = moviesData.Search.map((movie) =>
-    movieHTML(movie)
-  ).join('');
+const filterSelectEl = document.querySelector("#filter");
+filterSelectEl.addEventListener("change", () => {
+    const searchTerm = document.querySelector(".search__input").value;
+    renderMovies(searchTerm);
+});
+
+async function renderMovies(searchTerm) {
+    moviesListEl.classList += ' movies__loading';
+    const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=94c6a856`);
+    const moviesData = await response.json();
+    let movies = moviesData.Search || [];
+
+    setTimeout(() => {
+        moviesListEl.classList.remove('movies__loading');
+        displayMovies(movies);
+        setupFilters(movies);
+    }, 1000);
+}
+
+function displayMovies(movies) {
+    moviesListEl.innerHTML = movies.map(movie => movieHTML(movie)).join('');
+}
+
+function filterMovies() {
+    const selectElement = document.querySelector("#filter");
+    return selectElement.value;
+}
+
+function setupFilters(movies) {
+    const filter = filterMovies();
+
+    if (filter === "OLD_TO_NEW") {
+        movies.sort((a, b) => Number(a.Year) - Number(b.Year));
+    } else if (filter === "NEW_TO_OLD") {
+        movies.sort((a, b) => Number(b.Year) - Number(a.Year));
+    }
+
+    displayMovies(movies);
 }
 
 function movieHTML(movie) {
-  return `<div class="movies-card">
-            <div class="movie-card__container">
-                <h3>${movie.Title}</h4>
-                    <p><b>Year:</b> ${movie.Year}</p>
-                     <p><b>Poster:</b> 
-                     <img src="${movie.Poster}" alt=""></p>
-                     <p>
-            </div>
-        </div>`
+    return `
+        <div class="movie">
+            <figure class="movie__img--wrapper">
+                <img class="movie__img" src="${movie.Poster}" alt="">
+            </figure>
+            <div class="movie__title">${movie.Title}</div>
+            <div class="movie__price">${movie.Year}</div>
+        </div>`;
 }
 
-renderMovies(id);
+function openMenu() {
+    document.body.classList += " menu--open";
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open');
+}
